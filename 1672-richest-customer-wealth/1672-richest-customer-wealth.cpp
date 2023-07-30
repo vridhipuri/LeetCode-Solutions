2d0984b77c5d04d03c59fe6d3fd66e0e6bc6be04
Vridhi Puri <piscespuri22@gmail.com>
2023-07-31 00:52:27 +0530

    Time: 6 ms (47.72%), Space: 7.8 MB (61.53%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+
+        int n=accounts.size();
+        int m=accounts[0].size();
+        int maxi=INT_MIN;
+
+        for(int i=0;i<n;i++)
+        {
+            int sum=0;
+
+            for(int j=0;j<m;j++)
+            {
+                sum+=accounts[i][j];
+            }
+            maxi=max(maxi,sum);
+        }
+        return maxi;
+    }
+};
