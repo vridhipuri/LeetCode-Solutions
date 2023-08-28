46dec9cb9978b2f4c21ce0a67842548bd10f7357
Vridhi Puri <piscespuri22@gmail.com>
2023-08-29 02:33:26 +0530

    Time: 5 ms (41.65%), Space: 7.2 MB (33.33%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        
+        int n=str1.length();
+        int m=str2.length();
+        if(str1+str2!=str2+str1)
+        {
+            return "";
+        }
+        return str1.substr(0,gcd(n,m));
+    }
+};
